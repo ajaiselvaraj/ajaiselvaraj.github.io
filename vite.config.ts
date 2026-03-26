5336d8e0132c1418138fb3ae1e70cdcdb2bb10e7
ajai1422 <ajaiakshai123@gmail.com>
2026-03-26 09:47:35 +0530

    sitemaps

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import prerender from '@prerenderer/rollup-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    prerender({
-      routes: ['/'],
-      renderer: '@prerenderer/renderer-puppeteer',
-      server: {
-        host: 'localhost',
-        port: 5173
-      }
-    })
-  ],
+  plugins: [react()],
 })
